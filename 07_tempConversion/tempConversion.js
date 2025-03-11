c1b7897b4bd45f68745613ead2496e1f8c8b8db7
Sergio <Sergiopadilla0987@gmail.com>
2025-03-11 06:34:38 -0400

    completed javascript exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = ((9/5 * temp) + 32).toFixed(1);
+  return parseFloat(conversion);
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (5/9 * (temp - 32)).toFixed(1);
+  return parseFloat(conversion);
 };
 
 // Do not edit below this line
